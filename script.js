4a1e483342bf682a964dcbb6f6329724b9bb79c6
Arjun <arjunsemwal02@gmail.com>
2024-11-28 18:39:18 +0530

    added the win state
    browser gives alert when player wins or game draws

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const player = ['O', 'X']
+const players = ['O', 'X']
 const gameBoard = [
     '', '', '',
     '', '', '',
@@ -23,13 +23,66 @@ const createGameBoardElement = () => {
 const createSquareElement = squareNumber => {
     const squareElement = document.createElement('div')
     squareElement.classList.add('game-square')
+
+    squareElement.addEventListener('click', (event) => {
+        const {target} = event
+        target.textContent = currentPlayer
+        gameBoard[squareNumber] = currentPlayer
+        checkBoard()
+        switchPlayer()
+    }, {once: true})
+
     return squareElement
+}
+
+const switchPlayer = () => {
+    if(currentPlayer === players[0])   currentPlayer = players[1]
+    else currentPlayer = players[0]
+}
+
+const checkBoard = () => {
+    // game Board
+    // [
+    //     ['0', '1', '2'],
+    //     ['3', '4', '5'],
+    //     ['6', '7', '8'],
+    // ]
+
+    const winStates = [
+        ['0', '1', '2'],
+        ['3', '4', '5'],
+        ['6', '7', '8'],
+        ['0', '3', '6'],
+        ['1', '4', '7'],
+        ['2', '5', '8'],
+        ['0', '4', '8'],
+        ['6', '4', '2'],
+    ]
+
+    for( let winState of winStates){
+        const [position1, position2, position3] = winState
+
+        if(gameBoard[position1] !== '' &&
+            gameBoard[position1] === gameBoard[position2] &&
+            gameBoard[position1] === gameBoard[position3]
+        ){
+            alert(`${gameBoard[position1]}'s wins`)
+            return
+        }
+    }
+
+    const allSquaresUsed = gameBoard.every(square => square !== '')
+
+    if(allSquaresUsed) {
+        alert(`It's a draw!`)
+    }
 }
 
 const resetGame = () => {
     gameBoardElement = createGameBoardElement()
     for(let square = 0; square < 9; square++)
         gameBoardElement.appendChild(createSquareElement(square))
+    currentPlayer = players[0]
     document.body.appendChild(gameBoardElement)
 }
 
